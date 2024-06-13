0322ed9a41a387dadb5f76bb6caf28d03a08bc9e
Tianmin Lyu <36526042+thecrossed@users.noreply.github.com>
2024-06-13 21:30:58 +0200

    Update ratings.py

--- src/ratings.py
+++ src/ratings.py
@@ -15,7 +15,7 @@ students_username = ['yaohengli',
            'Jasminezhao777',
            'Justinzhao777',
            'QuantumInnovator',
-           'Newinnovator',
+           'Spade',
            'Jason-Ma',
            
            'chessloverma',
